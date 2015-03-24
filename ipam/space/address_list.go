3b01550c0901fe35bfa8d98765f6328ff645416f
Tom Wilkie <tom@weave.works>
2015-03-24 15:39:15 +0000

    Make address list use sort.Search for binary searches

--- ipam/space/address_list.go
+++ ipam/space/address_list.go
@@ -3,35 +3,54 @@ package space
 import (
 	"github.com/zettio/weave/ipam/utils"
 	"net"
+	"sort"
 )
 
+// For compatibility with Sorting
 type addressList []net.IP
+
+func (as addressList) Len() int           { return len(as) }
+func (as addressList) Less(i, j int) bool { return utils.Ip4int(as[i]) < utils.Ip4int(as[j]) }
+func (as addressList) Swap(i, j int)      { panic("Should never be swapping entries!") }
+
 
 // Maintain addresses in increasing order.
 func (aa *addressList) add(a net.IP) {
-	for i, b := range *aa {
-		if utils.Subtract(b, a) > 0 {
-			(*aa) = append((*aa), nil)   // make space
-			copy((*aa)[i+1:], (*aa)[i:]) // move up
-			(*aa)[i] = a                 // put in new element
-			return
-		}
-	}
-	*aa = append(*aa, a)
+	utils.Assert(sort.IsSorted(*aa), "address list must always be sorted")
+
+	i := sort.Search(len(*aa), func(j int) bool {
+		return utils.Ip4int((*aa)[j]) >= utils.Ip4int(a)
+	})
+
+	utils.Assert(i >= len(*aa) || !(*aa)[i].Equal(a), "inserting address into list already exists!")
+
+	(*aa) = append((*aa), nil)   // make space
+	copy((*aa)[i+1:], (*aa)[i:]) // move up
+	(*aa)[i] = a                 // put in new element
+
+	utils.Assert(sort.IsSorted(aa), "address list must always be sorted")
 }
 
+// Seems to be unused?
 func (aa *addressList) removeAt(pos int) {
 	// Delete, preserving order
 	(*aa) = append((*aa)[:pos], (*aa)[pos+1:]...)
 }
 
 func (aa *addressList) find(addr net.IP) int {
-	for i, a := range *aa {
-		if a.Equal(addr) {
-			return i
-		}
+	utils.Assert(sort.IsSorted(*aa), "address list must always be sorted")
+
+	i := sort.Search(len(*aa), func(j int) bool {
+		return utils.Ip4int((*aa)[j]) >= utils.Ip4int(addr)
+	})
+
+	if i >= len(*aa) || !(*aa)[i].Equal(addr) {
+		// this it not idomatic go; we should set err
+		// and return i (as the place said address might go)
+		return -1
 	}
-	return -1
+
+	return i
 }
 
 func (aa *addressList) take() net.IP {
