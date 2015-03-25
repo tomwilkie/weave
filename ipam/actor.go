44f34543f280bcde32dbbd523bbf95124d7e98b9
Bryan Boreham <bjboreham@gmail.com>
2015-03-25 13:48:54 +0000

    Refactored Actor to pass closures rather than 'interaction' structs

--- ipam/actor.go
+++ ipam/actor.go
@@ -2,68 +2,43 @@ package ipam
 
 import (
 	"fmt"
-	lg "github.com/zettio/weave/common"
 	"github.com/zettio/weave/router"
 	"net"
 )
 
-// Types used to send requests from Actor client to server
-type stop struct{}
-type makeString struct {
-	resultChan chan<- string
-}
-type getFor struct {
-	resultChan chan<- net.IP
-	Ident      string
-}
-type cancelGetFor struct {
-	Ident string
-}
-type free struct {
-	resultChan chan<- error
-	Ident      string
-	IP         net.IP
-}
-type deleteRecordsFor struct {
-	Ident string
-}
-type gossipUnicast struct {
-	resultChan chan<- error
-	sender     router.PeerName
-	bytes      []byte
-}
-type gossipBroadcast struct {
-	resultChan chan<- error
-	bytes      []byte
-}
-type gossipEncode struct {
-	resultChan chan<- []byte
-}
-type gossipReply struct {
-	err    error
-	update router.GossipData
-}
-type gossipUpdate struct {
-	resultChan chan<- gossipReply
-	bytes      []byte
+func (alloc *Allocator) Start() {
+	actionChan := make(chan interface{}, router.ChannelSize)
+	alloc.actionChan = actionChan
+	go alloc.actorLoop(actionChan, true)
 }
 
 // Actor client API
 
-// Sync.
+type stop struct{}
+
+// Async.
 func (alloc *Allocator) Stop() {
-	alloc.queryChan <- stop{}
+	alloc.actionChan <- stop{}
 }
 
 // Sync.
 func (alloc *Allocator) GetFor(ident string, cancelChan <-chan bool) net.IP {
 	resultChan := make(chan net.IP)
-	alloc.queryChan <- getFor{resultChan, ident}
+	alloc.actionChan <- func() {
+		alloc.electLeaderIfNecessary()
+		if addrs, found := alloc.owned[ident]; found && len(addrs) > 0 {
+			resultChan <- addrs[0] // currently not supporting multiple allocations in the same subnet
+		} else if !alloc.tryAllocateFor(ident, resultChan) {
+			alloc.pending = append(alloc.pending, pendingAllocation{resultChan, ident})
+		}
+	}
 	select {
 	case result := <-resultChan:
 		return result
 	case <-cancelChan:
-		alloc.queryChan <- cancelGetFor{ident}
+		alloc.actionChan <- func() {
+			alloc.handleCancelGetFor(ident)
+		}
 		return nil
 	}
 }
@@ -71,20 +46,33 @@ func (alloc *Allocator) GetFor(ident string, cancelChan <-chan bool) net.IP {
 // Sync.
 func (alloc *Allocator) Free(ident string, addr net.IP) error {
 	resultChan := make(chan error)
-	alloc.queryChan <- free{resultChan, ident, addr}
+	alloc.actionChan <- func() {
+		if alloc.removeOwned(ident, addr) {
+			resultChan <- alloc.spaceSet.Free(addr)
+		} else {
+			resultChan <- fmt.Errorf("free: %s not owned by %s", addr, ident)
+		}
+	}
 	return <-resultChan
 }
 
 // Sync.
 func (alloc *Allocator) String() string {
 	resultChan := make(chan string)
-	alloc.queryChan <- makeString{resultChan}
+	alloc.actionChan <- func() {
+		resultChan <- alloc.string()
+	}
 	return <-resultChan
 }
 
 // Async.
 func (alloc *Allocator) DeleteRecordsFor(ident string) error {
-	alloc.queryChan <- deleteRecordsFor{ident}
+	alloc.actionChan <- func() {
+		for _, ip := range alloc.owned[ident] {
+			alloc.spaceSet.Free(ip)
+		}
+		delete(alloc.owned, ident)
+	}
 	return nil // this is to satisfy the ContainerObserver interface
 }
 
@@ -92,7 +80,16 @@ func (alloc *Allocator) DeleteRecordsFor(ident string) error {
 func (alloc *Allocator) OnGossipUnicast(sender router.PeerName, msg []byte) error {
 	alloc.Debugln("OnGossipUnicast from", sender, ": ", len(msg), "bytes")
 	resultChan := make(chan error)
-	alloc.queryChan <- gossipUnicast{resultChan, sender, msg}
+	alloc.actionChan <- func() {
+		switch msg[0] {
+		case msgLeaderElected:
+			resultChan <- alloc.handleLeaderElected()
+		case msgSpaceRequest:
+			// some other peer asked us for space
+			alloc.donateSpace(sender)
+			resultChan <- nil
+		}
+	}
 	return <-resultChan
 }
 
@@ -100,92 +97,53 @@ func (alloc *Allocator) OnGossipUnicast(sender router.PeerName, msg []byte) erro
 func (alloc *Allocator) OnGossipBroadcast(msg []byte) error {
 	alloc.Debugln("OnGossipBroadcast:", len(msg), "bytes")
 	resultChan := make(chan error)
-	alloc.queryChan <- gossipBroadcast{resultChan, msg}
+	alloc.actionChan <- func() {
+		resultChan <- alloc.ring.OnGossipBroadcast(msg)
+		alloc.considerNewSpaces()
+		alloc.considerOurPosition()
+	}
 	return <-resultChan
 }
 
 // Sync.
 func (alloc *Allocator) Encode() []byte {
 	resultChan := make(chan []byte)
-	alloc.queryChan <- gossipEncode{resultChan}
+	alloc.actionChan <- func() {
+		resultChan <- alloc.ring.GossipState()
+	}
 	return <-resultChan
 }
 
 // Sync.
 func (alloc *Allocator) OnGossip(msg []byte) (router.GossipData, error) {
 	alloc.Debugln("Allocator.OnGossip:", len(msg), "bytes")
-	resultChan := make(chan gossipReply)
-	alloc.queryChan <- gossipUpdate{resultChan, msg}
-	ret := <-resultChan
-	return ret.update, ret.err
+	resultChan := make(chan error)
+	alloc.actionChan <- func() {
+		err := alloc.ring.OnGossipBroadcast(msg)
+		resultChan <- err
+		alloc.considerOurPosition()
+	}
+	err := <-resultChan
+	return nil, err // for now, we never propagate updates. TBD
 }
 
 // ACTOR server
 
-func (alloc *Allocator) queryLoop(queryChan <-chan interface{}, withTimers bool) {
+func (alloc *Allocator) actorLoop(actionChan <-chan interface{}, withTimers bool) {
+	// FIXME: not doing any timers at the moment.
 	for {
 		select {
-		case query, ok := <-queryChan:
+		case query, ok := <-actionChan:
 			if !ok {
 				return
 			}
 			switch q := query.(type) {
+			case func():
+				q()
 			case stop:
 				return
-			case makeString:
-				q.resultChan <- alloc.string()
-			case getFor:
-				alloc.electLeaderIfNecessary()
-				if addrs, found := alloc.owned[q.Ident]; found && len(addrs) > 0 {
-					q.resultChan <- addrs[0] // currently not supporting multiple allocations in the same subnet
-				} else if !alloc.tryAllocateFor(q.Ident, q.resultChan) {
-					alloc.pending = append(alloc.pending, getFor{q.resultChan, q.Ident})
-				}
-			case cancelGetFor:
-				alloc.handleCancelGetFor(q.Ident)
-			case deleteRecordsFor:
-				for _, ip := range alloc.owned[q.Ident] {
-					alloc.spaceSet.Free(ip)
-				}
-				delete(alloc.owned, q.Ident)
-			case free:
-				if alloc.removeOwned(q.Ident, q.IP) {
-					q.resultChan <- alloc.spaceSet.Free(q.IP)
-				} else {
-					q.resultChan <- fmt.Errorf("free: %s not owned by %s", q.IP, q.Ident)
-				}
-			case gossipUnicast:
-				switch q.bytes[0] {
-				case msgLeaderElected:
-					q.resultChan <- alloc.handleLeaderElected()
-				case msgSpaceRequest:
-					// some other peer asked us for space
-					alloc.donateSpace(q.sender)
-					q.resultChan <- nil
-				}
-			case gossipBroadcast:
-				q.resultChan <- alloc.ring.OnGossipBroadcast(q.bytes)
-				alloc.considerNewSpaces()
-				alloc.considerOurPosition()
-			case gossipUpdate:
-				err := alloc.ring.OnGossipBroadcast(q.bytes)
-				updateSet := (*ipamGossipData)(nil) // fixme
-				q.resultChan <- gossipReply{err, updateSet}
-				alloc.considerOurPosition()
-			case gossipEncode:
-				q.resultChan <- alloc.ring.GossipState()
 			}
 		}
 		alloc.assertInvariants()
 	}
-}
-
-func (alloc *Allocator) Errorln(args ...interface{}) {
-	lg.Error.Println(append([]interface{}{fmt.Sprintf("[allocator %s]:", alloc.ourName)}, args...)...)
-}
-func (alloc *Allocator) Infof(fmt string, args ...interface{}) {
-	lg.Info.Printf("[allocator %s] "+fmt, append([]interface{}{alloc.ourName}, args...)...)
-}
-func (alloc *Allocator) Debugln(args ...interface{}) {
-	lg.Debug.Println(append([]interface{}{fmt.Sprintf("[allocator %s]:", alloc.ourName)}, args...)...)
 }
