bfd17ed6785a5083719d6743826327fdd666bd91
Bryan Boreham <bjboreham@gmail.com>
2015-05-13 18:06:01 +0100

    Update ipam unit tests in line with master

--- test/120_ipam_test.sh
+++ test/120_ipam_test.sh
@@ -6,20 +6,13 @@ UNIVERSE=10.2.1.0/24
 
 start_suite "Ping over cross-host weave network with IPAM"
 
-for HOST in $HOST1 $HOST2; do
-    weave_on $HOST stop || true
-    weave_on $HOST stop-dns || true
-    docker_on $HOST rm -f c1 c2 || true
-done
-
 weave_on $HOST1 launch -iprange $UNIVERSE
 weave_on $HOST2 launch -iprange $UNIVERSE $HOST1
 
-weave_on $HOST1 run -t --name=c1 ubuntu
-weave_on $HOST2 run -t --name=c2 ubuntu
+weave_on $HOST1 run -t --name=c1 gliderlabs/alpine /bin/sh
+weave_on $HOST2 run -t --name=c2 gliderlabs/alpine /bin/sh
 # Note can't use weave_on here because it echoes the command
 C2IP=$(DOCKER_HOST=tcp://$HOST2:2375 $WEAVE ps | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
-ok=$(docker -H tcp://$HOST1:2375 exec -i c1 sh -c "ping -q -c 4 $C2IP >&2 && echo ok")
-assert "echo $ok" "ok"
+assert_raises "exec_on $HOST1 c1 ping -q -c 4 $C2IP"
 
 end_suite
--- test/250_dns_and_ipam.sh
+++ test/250_dns_and_ipam.sh
@@ -6,19 +6,13 @@ UNIVERSE=10.2.3.0/24
 
 start_suite "Resolve names over cross-host weave network with IPAM"
 
-for host in $HOST1 $HOST2; do
-    weave_on $host stop || true
-    weave_on $host stop-dns || true
-    docker_on $host rm -f c1 c2 || true
-done
-
 weave_on $HOST1 launch -iprange $UNIVERSE
 weave_on $HOST2 launch -iprange $UNIVERSE $HOST1
 
 weave_on $HOST1 launch-dns 10.2.254.1/24
 weave_on $HOST2 launch-dns 10.2.254.2/24
 
-weave_on $HOST2 run -t --name=c2 -h seetwo.weave.local ubuntu
+weave_on $HOST2 run -t --name=c2 -h seetwo.weave.local gliderlabs/alpine /bin/sh
 weave_on $HOST1 run --with-dns --name=c1 -t aanand/docker-dnsutils /bin/sh
 
 # Note can't use weave_on here because it echoes the command
